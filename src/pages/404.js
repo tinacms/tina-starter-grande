9f851d28a4b6b9d0d976a6acd3ca6b11d23acf6a
Scott Byrne <scott.byrne@forestry.io>
2019-10-22 10:30:33 -0300

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,16 @@ import React from "react"
 import { SEO } from "../components/seo"
 import Layout from "../components/layout"
 import { Paper } from "../components/style"
+import Layout from "../components/layout"
 
 const NotFoundPage = () => (
-  <>
+  <LayoutDimensions>
     <SEO title="404: Not found" />
     <Paper>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Paper>
-  </>
+  </LayoutDimensions>
 )
 
 export default NotFoundPage
