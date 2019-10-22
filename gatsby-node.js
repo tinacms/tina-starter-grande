00657a9bc25e840885fe110fbf71305117dea7d8
Scott Byrne <scott.byrne@forestry.io>
2019-10-22 14:11:59 -0300

    remove unused node script

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,17 +33,3 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     })
   })
 }
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
