e0b021242186e32cd9d9bb4de0cfa01f346bbcbb
Scott Byrne <scott.byrne@forestry.io>
2019-10-22 10:33:59 -0300

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { SEO } from "../components/seo"
-import Layout from "../components/layout"
 import { Paper } from "../components/style"
 import Layout from "../components/layout"
 
