fd9ab68b591e8b9de330494bc38e36bacd997028
DJ <dj@walker.dev>
2019-10-30 12:51:47 -0400

    query for block html

--- src/templates/page.js
+++ src/templates/page.js
@@ -18,7 +18,7 @@ function Page(props) {
         <h2>{page.title}</h2>
         <hr />
         {blocks &&
-          blocks.map(({ _template, ...data }) => {
+          blocks.map(({ _template, ...data }, i) => {
             {
               console.log(_template)
               console.log(data)
@@ -27,7 +27,7 @@ function Page(props) {
               case "FormBlock":
                 return <Form form={data} />
               case "ContentBlock":
-                return <p>test</p>
+                return <div dangerouslySetInnerHTML={{__html: page.childrenPagesJsonBlockMarkdown[i].childMarkdownRemark.html}}></div>
               default:
                 return "Error"
             }
@@ -74,7 +74,11 @@ export const pageQuery = graphql`
           autocomplete
         }
       }
-
+      childrenPagesJsonBlockMarkdown {
+        childMarkdownRemark {
+          html
+        }
+      }
       rawJson
       fileRelativePath
     }
