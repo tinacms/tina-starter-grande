e8d811646a49caa8e182904edf6bd66ed9bb48b3
Scott Byrne <scott.byrne@forestry.io>
2019-11-27 12:53:44 -0400

    bring menu from blocks, get add author working

--- src/fields/authors.js
+++ src/fields/authors.js
@@ -1,25 +1,110 @@
-import React from "react"
+import React, { useState } from "react"
+import styled, { css } from "styled-components"
 import { FieldMeta } from "tinacms"
 import authorsJson from "../../content/settings/authors"
 
 export const AuthorsField = props => {
-  const authors = authorsJson.authors
+  const { input, field, form, meta } = props
 
-  const authorSlugs = props.input.value || []
+  const addAuthor = React.useCallback(
+    slug => {
+      form.mutators.insert(field.name, 0, slug)
+    },
+    [field.name, form.mutators]
+  )
+
+  const [visible, setVisible] = React.useState(false)
+  const authors = authorsJson.authors
+  const authorSlugs = input.value || []
 
   return (
-    <FieldMeta
-      name={props.input.name}
-      label={props.field.label}
-      description={props.field.description}
-      error={props.meta.error}
-    >
-      {authorSlugs.map(authorSlug => {
-        const author = authors.find(author => author.slug === authorSlug)
-        return <p>{author.name}</p>
-      })}
-    </FieldMeta>
+    <>
+      <AuthorsHeader>
+        <button onClick={() => setVisible(!visible)} open={visible}>
+          Add Author
+        </button>
+        <BlockMenu open={visible}>
+          <BlockMenuList>
+            {authors.map(author => (
+              <BlockOption
+                onClick={() => {
+                  addAuthor(author.slug)
+                  setVisible(false)
+                }}
+              >
+                {author.name}
+              </BlockOption>
+            ))}
+          </BlockMenuList>
+        </BlockMenu>
+      </AuthorsHeader>
+      <FieldMeta
+        name={input.name}
+        label={field.label}
+        description={field.description}
+        error={meta.error}
+      >
+        {authorSlugs.map(authorSlug => {
+          const author = authors.find(author => author.slug === authorSlug)
+          return <p>{author ? author.name : "error"}</p>
+        })}
+      </FieldMeta>
+    </>
   )
 }
 
 const EmptyState = () => <p>There's no authors</p>
+
+const AuthorsHeader = styled.div`
+  position: relative;
+`
+
+const BlockMenu = styled.div`
+  min-width: 12rem;
+  border-radius: 0.25rem;
+  border: 1px solid #efefef;
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate3d(0, 0, 0) scale3d(0.5, 0.5, 1);
+  opacity: 0;
+  pointer-events: none;
+  transition: all 150ms ease-out;
+  transform-origin: 100% 0;
+  background-color: white;
+  overflow: hidden;
+  z-index: 100;
+  ${props =>
+    props.open &&
+    css`
+      opacity: 1;
+      pointer-events: all;
+      transform: translate3d(0, 2.25rem, 0) scale3d(1, 1, 1);
+    `};
+`
+
+const BlockMenuList = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const BlockOption = styled.button`
+  position: relative;
+  text-align: center;
+  font-size: 0.8rem;
+  padding: 0.75rem;
+  font-weight: 500;
+  width: 100%;
+  background: none;
+  cursor: pointer;
+  outline: none;
+  border: 0;
+  transition: all 85ms ease-out;
+  &:hover {
+    background-color: #f6f6f9;
+  }
+  &:not(:last-child) {
+    border-bottom: 1px solid #efefef;
+  }
+`
