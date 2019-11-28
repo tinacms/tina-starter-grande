2a6afa574f512757be3ab62f8bbf7b2fa661009f
Scott Byrne <scott.byrne@forestry.io>
2019-11-28 09:08:53 -0400

    fix build issue

--- src/templates/post.js
+++ src/templates/post.js
@@ -32,7 +32,7 @@ function Post(props) {
       <Paper>
         <Meta>
           <MetaSpan>{page.frontmatter.date}</MetaSpan>
-          {page.frontmatter.authors.length > 0 && (
+          {page.frontmatter.authors && page.frontmatter.authors.length > 0 && (
             <MetaSpan>
               <em>By</em>&nbsp;
               <ListAuthors authorIDs={page.frontmatter.authors} />
