932086346a6348ab45cc75ac47b90bc35ff97db7
Scott Byrne <spbyrne@outlook.com>
2020-05-29 15:58:11 -0300

    Revert "refactor(markdown in json): replace PagesJsonBlockMarkdown with gatsby-plugin-json-remark"

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,6 +27,82 @@ exports.setFieldsOnGraphQLNodeType = ({ type }) => {
   return {}
 }
 
+exports.onCreateNode = ({
+  node,
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode, createNodeField, createParentChildLink } = actions
+
+  // Check for the correct type to only affect this
+  if (node.internal.type === `PagesJson`) {
+    // transform markdown in blocks[i].content
+    if (node.blocks) {
+      const markdownHost = {
+        id: createNodeId(`${node.id} markdown host`),
+        parent: node.id,
+        internal: {
+          contentDigest: createContentDigest(JSON.stringify(node.blocks)),
+          type: `${node.internal.type}MarkdownData`,
+        },
+      }
+
+      createNode(markdownHost)
+
+      createNodeField({
+        node,
+        name: `markdownContent___NODE`, // Before the ___NODE: Name of the new fields
+        value: markdownHost.id, // Connects both nodes
+      })
+
+      node.blocks.forEach((block, i) => {
+        if (!block.content) {
+          block.content = ""
+        }
+        const blockNode = {
+          id: `${node.id} block ${i} markdown`,
+          parent: markdownHost.id,
+          internal: {
+            content: block.content,
+            contentDigest: createContentDigest(block.content),
+            type: `${node.internal.type}BlockMarkdown`,
+            mediaType: "text/markdown",
+          },
+        }
+
+        createNode(blockNode)
+
+        createParentChildLink({ parent: node, child: blockNode })
+      })
+    }
+
+    // transform markdown in node.content
+    if (node.content) {
+      const textNode = {
+        id: createNodeId(`${node.id} markdown field`),
+        children: [],
+        parent: node.id,
+        internal: {
+          content: node.content,
+          mediaType: `text/markdown`, // Important!
+          contentDigest: createContentDigest(node.content),
+          type: `${node.internal.type}Markdown`,
+        },
+      }
+
+      createNode(textNode)
+
+      // Add link to the new node
+      createNodeField({
+        node,
+        name: `markdownContent___NODE`, // Before the ___NODE: Name of the new fields
+        value: textNode.id, // Connects both nodes
+      })
+    }
+  }
+}
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
